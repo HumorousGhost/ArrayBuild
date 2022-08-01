9b3483f0bf5e03b7b979970032432d35f4620a93
v-zhangliqun <v-zhangliqun@360.cn>
2022-08-01 10:59:36 +0800

    first

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
 # ArrayBuild
 
-A description of this package.
+Custom `ArrayBuilder` with `resultBuilder`
+
+## Usage
+
+```swift
+import ArrayBuilder
+
+let someNumber = 19
+@ArrayBuilder<Int> var builder: [Int] {
+    if someNumber < 12 {
+        31
+    } else if someNumber == 19 {
+        32
+    } else {
+        33
+    }
+}
+
+// print
+print(builder)
+// [32]
+```
+
+
+## Installation
+
+You can add KeyboardHost to an Xcode project by adding it as a package dependency.
+
+From the **File** menu, select **Swift Packages** › **Add Package Dependency…**
+Enter https://github.com/HumorousGhost/ArrayBuild into the package repository URL text field
+Link **KeyboardHost** to your application target
--- Sources/ArrayBuild/ArrayBuild.swift
+++ Sources/ArrayBuild/ArrayBuild.swift
@@ -1,6 +1,59 @@
-public struct ArrayBuild {
-    public private(set) var text = "Hello, World!"
+//
+//  ArrayBuild.swift
+//  ListDemo
+//
+//  Created by HumorousGhost on 2022/7/15.
+//
 
-    public init() {
+import Foundation
+
+public extension Array {
+    /// Builds an array from an @ArrayBuilder closure. Use `Array.build` if you're running into ambiguity issues.
+    init(@ArrayBuilder<Element> contents: () throws -> [Element]) rethrows {
+        self = try contents()
+    }
+    
+    /// Builds an array from an @ArrayBuilder closure. Provided in addition to `init(contents:)` to avoid ambiguity.
+    static func build(@ArrayBuilder<Element> contents: () throws -> [Element]) rethrows -> Self {
+        try Self(contents: contents)
+    }
+}
+
+@resultBuilder
+public enum ArrayBuilder<Element> {
+    public typealias Expression = Element
+
+    public typealias Component = [Element]
+
+    public static func buildExpression(_ expression: Expression) -> Component {
+        [expression]
+    }
+
+    public static func buildExpression(_ expression: Expression?) -> Component {
+        expression.map({ [$0] }) ?? []
+    }
+
+    public static func buildBlock(_ children: Component...) -> Component {
+        children.flatMap({ $0 })
+    }
+
+    public static func buildOptional(_ children: Component?) -> Component {
+        children ?? []
+    }
+
+    public static func buildBlock(_ component: Component) -> Component {
+        component
+    }
+
+    public static func buildEither(first child: Component) -> Component {
+        child
+    }
+
+    public static func buildEither(second child: Component) -> Component {
+        child
+    }
+    
+    public static func buildArray(_ contents: [[Element]]) -> [Element] {
+        Array(contents.joined())
     }
 }
